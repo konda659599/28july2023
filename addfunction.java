dbc051f2ebed3a2b543ad58d0c544721f78098f0
manju <rkoda375@gmail.com>
2023-07-24 22:14:28 +0530

    my task completes

--- addfunction.java
+++ addfunction.java
@@ -1,1 +1,10 @@
+<<<<<<< Updated upstream
  my manager work completed
+=======
+1st day java lines of code complete
+2st day java lines of code complete
+3st day java lines of code complete
+4st day java lines of code complete
+5th day java 
+
+>>>>>>> Stashed changes
